58f38881700ad20969e25bd94c8311154dad09f1
roopen <roopen@users.noreply.github.com>
2025-05-18 01:44:46 +0300

    tests: test project added, first tests

--- LocalAIAgent.App/News/NewsItem.cs
+++ LocalAIAgent.App/News/NewsItem.cs
@@ -5,13 +5,13 @@ namespace Local_AI_Agent.News
     public class NewsItem
     {
         public DateTimeOffset PublishDate { get; }
-        public string Title { get; }
+        public string? Title { get; }
         public string? Summary { get; }
         public string? Link { get; }
 
         public NewsItem(SyndicationItem syndicationItem)
         {
-            Title = syndicationItem.Title.Text;
+            Title = syndicationItem.Title?.Text;
             Summary = syndicationItem.Summary?.Text;
             PublishDate = syndicationItem.PublishDate;
             Link = syndicationItem.Links.FirstOrDefault()?.Uri.ToString();
