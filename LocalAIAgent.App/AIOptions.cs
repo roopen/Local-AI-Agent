e2f30735810655d244fa227400aae4a6447c7d0d
roopen <roopen@users.noreply.github.com>
2025-05-18 12:50:14 +0300

    feat: configure AI settings

---
+++ LocalAIAgent.App/AIOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace LocalAIAgent.App
+{
+    internal class AIOptions
+    {
+        public required string ModelId { get; set; }
+        public required string EndpointUrl { get; set; }
+        public string ApiKey { get; set; } = string.Empty;
+
+        public decimal Temperature { get; set; }
+        public decimal TopP { get; set; }
+        public decimal FrequencyPenalty { get; set; }
+        public decimal PresencePenalty { get; set; }
+    }
+}
--- LocalAIAgent.App/Program.cs
+++ LocalAIAgent.App/Program.cs
@@ -1,33 +1,46 @@
 ﻿using Local_AI_Agent.Chat;
 using Local_AI_Agent.News;
 using Local_AI_Agent.Time;
+using LocalAIAgent.App;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 
-IKernelBuilder kernelBuilder = Kernel.CreateBuilder()
-    .AddOpenAIChatCompletion(
-        modelId: "gemma-3-27b-it-abliterated",
-        apiKey: string.Empty,
-        endpoint: new Uri("http://localhost:1234/v1/")
-    );
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
 
 kernelBuilder.Services.AddYleNewsClient();
 kernelBuilder.Services.AddFoxNewsClient();
 kernelBuilder.Services.AddSingleton<ChatService>();
 
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+    .Build();
+
+kernelBuilder.Services.Configure<AIOptions>(configuration.GetSection("AIOptions"));
+AIOptions? aiOptions = configuration.GetSection("AIOptions").Get<AIOptions>() ?? throw new Exception("AIOptions not found in configuration.");
+
 kernelBuilder.Plugins.AddFromType<TimeService>();
 kernelBuilder.Plugins.AddFromType<NewsService>();
 
+kernelBuilder
+    .AddOpenAIChatCompletion(
+        modelId: aiOptions.ModelId,
+        apiKey: aiOptions.ApiKey,
+        endpoint: new Uri(aiOptions.EndpointUrl)
+    );
+
 Kernel kernel = kernelBuilder.Build();
 
-await StartAiChat(kernel);
+await StartAiChat(kernel, aiOptions);
 
-static async Task StartAiChat(Kernel kernel)
+static async Task StartAiChat(Kernel kernel, AIOptions options)
 {
     ChatService chatService = new(
         kernel.Services.GetService<IChatCompletionService>()!,
-        kernel
+        kernel,
+        options
     );
     await chatService.StartChat();
 }
