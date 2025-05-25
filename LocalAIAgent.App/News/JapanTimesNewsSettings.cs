e03bbeabde482ebf093e5bd7210fbf3ec27c8866
roopen <roopen@users.noreply.github.com>
2025-05-25 11:32:15 +0300

    feat: Japan Times news feed

---
+++ LocalAIAgent.App/News/JapanTimesNewsSettings.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LocalAIAgent.App.News
+{
+    internal class JapanTimesNewsSettings : INewsClientSettings
+    {
+        public string ClientName => "JapanTimes";
+        public string UserAgent = "Mozilla/5.0";
+        public string Host = "japantimes.co.jp";
+        public string BaseUrl => "https://www.japantimes.co.jp";
+
+        public List<string> GetNewsUrls()
+        {
+            return
+                [
+                    $"{BaseUrl}/feed",
+                ];
+        }
+
+        public void AddHttpClient(IServiceCollection services)
+        {
+            services.AddHttpClient(ClientName, client =>
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+            })
+            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                AllowAutoRedirect = true
+            });
+        }
+    }
+}
