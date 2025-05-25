55a370a690b2bb3df95079b475ba64df8a423518
roopen <roopen@users.noreply.github.com>
2025-05-25 11:39:52 +0300

    refac: NewsSettings prettified

--- LocalAIAgent.App/News/YleNewsSettings.cs
+++ LocalAIAgent.App/News/YleNewsSettings.cs
@@ -5,23 +5,14 @@ namespace LocalAIAgent.App.News
     internal class YleNewsSettings : INewsClientSettings
     {
         public string ClientName => "YleNewsClient";
-        public string BaseUrl => "https://yle.fi/";
+        public string BaseUrl => "https://yle.fi/rss";
         public string UserAgent = "Mozilla/5.0";
-
-        public string MainHeadlinesUrl = "/rss/uutiset/paauutiset";
-        // These contain duplicate news. Needs to be handled separately.
-        //public static string FinanceNewsUrl = "rss/t/18-19274/fi";
-        //public static string WorldNewsUrl = "/rss/t/18-34953/fi";
-        //public static string FinlandNewsUrl = "/rss/t/18-34837/fi";
 
         public List<string> GetNewsUrls()
         {
             return
             [
-                MainHeadlinesUrl,
-                //FinanceNewsUrl,
-                //WorldNewsUrl,
-                //FinlandNewsUrl
+                $"{BaseUrl}/uutiset/paauutiset",
             ];
         }
 
