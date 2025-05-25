038d4e1b9b4e3516b8656c44f8f4ddf9b5ad9ae7
roopen <roopen@users.noreply.github.com>
2025-05-25 21:15:01 +0300

    tests: integration test for loading all news feeds

--- LocalAIAgent.ConsoleApp/Program.cs
+++ LocalAIAgent.ConsoleApp/Program.cs
@@ -1,17 +1,26 @@
-﻿using LocalAIAgent.ConsoleApp;
-using LocalAIAgent.SemanticKernel;
+﻿using LocalAIAgent.SemanticKernel;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.SemanticKernel;
 
-IHost host = Host.CreateDefaultBuilder()
-    .ConfigureServices((context, services) =>
+namespace LocalAIAgent.ConsoleApp
+{
+    public class Program
     {
-        services.AddSemanticKernel();
-    })
-.Build();
+        public static async Task Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSemanticKernel();
+                })
+            .Build();
 
-Kernel kernel = host.Services.GetRequiredService<Kernel>();
+            Kernel kernel = host.Services.GetRequiredService<Kernel>();
 
-await kernel.StartAIChatInConsole();
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") is not "IntegrationTests")
+                await kernel.StartAIChatInConsole();
+        }
+    }
+}
---
+++ LocalAIAgent.Tests/IntegrationTests/CustomApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using LocalAIAgent.ConsoleApp;
+using LocalAIAgent.SemanticKernel;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+
+namespace LocalAIAgent.Tests.IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "IntegrationTests", EnvironmentVariableTarget.Process);
+
+            builder.ConfigureServices(services =>
+            {
+                IKernelBuilder kernelBuilder = DependencyRegistrar.GetSemanticKernelBuilder();
+
+                kernelBuilder.Services.RemoveAll<IEmbeddingGenerator<string, Embedding<float>>>();
+                kernelBuilder.Services.AddSingleton<IEmbeddingGenerator<string, Embedding<float>>, Mocks.MockEmbeddingGenerator>();
+
+                services.AddSingleton(sp => kernelBuilder.Build());
+            });
+
+            return builder.Build();
+        }
+    }
+}
