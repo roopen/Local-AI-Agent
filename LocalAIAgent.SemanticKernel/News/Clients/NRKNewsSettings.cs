4889301916fbb77b72a31df5b9a3dae69ac1e6d0
roopen <roopen@users.noreply.github.com>
2025-07-06 13:28:17 +0300

    feat: NRK News

---
+++ LocalAIAgent.SemanticKernel/News/Clients/NRKNewsSettings.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LocalAIAgent.SemanticKernel.News.Clients
+{
+    internal class NRKNewsSettings : BaseNewsClientSettings
+    {
+        public override string ClientName => "NRKNewsClient";
+        public override string BaseUrl => "https://www.nrk.no";
+
+        public override bool RequiresTranslation => true;
+
+        public override List<string> GetNewsUrls()
+        {
+            return
+            [
+                $"{BaseUrl}/toppsaker.rss",
+                $"{BaseUrl}/viten/toppsaker.rss",
+            ];
+        }
+
+        public override void AddHttpClient(IServiceCollection services)
+        {
+            services.AddHttpClient(ClientName, client =>
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+                client.DefaultRequestHeaders.Host = Host;
+            })
+            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                AllowAutoRedirect = true
+            });
+        }
+    }
+}
