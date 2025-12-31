94ae59eb8a922a4baf1ee0821b73b1c514a4c3fa
roopen <roopen@users.noreply.github.com>
2025-12-31 20:00:13 +0200

    feat: ScienceDaily feed

---
+++ LocalAIAgent.SemanticKernel/News/Clients/ScienceDailyNewsSettings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LocalAIAgent.SemanticKernel.News.Clients
+{
+    internal class ScienceDailyNewsSettings : BaseNewsClientSettings
+    {
+        public override string ClientName => "ScienceDailyClient";
+        public override string BaseUrl => "https://www.sciencedaily.com/rss";
+
+        public override bool RequiresTranslation => false;
+
+        public override List<string> GetNewsUrls()
+        {
+            return
+            [
+                $"{BaseUrl}/all.xml",
+            ];
+        }
+
+        public override void AddHttpClient(IServiceCollection services)
+        {
+            services.AddHttpClient(ClientName, client =>
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+            });
+        }
+    }
+}
