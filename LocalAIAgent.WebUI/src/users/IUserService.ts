9bda0528148f22ea252f18a6a2c1f62b6b640561
roopen <roopen@users.noreply.github.com>
2026-02-01 15:48:14 +0200

    tests: fido2 related ui tests

--- LocalAIAgent.WebUI/src/users/IUserService.ts
+++ LocalAIAgent.WebUI/src/users/IUserService.ts
@@ -1,10 +1,9 @@
 import type { User } from "../domain/User";
 import type UserSettings from "../domain/UserSettings";
-import type { UserRegistrationDto } from "../clients/UserApiClient";
 
 export interface IUserService {
     login(): Promise<User | null>;
-    register(user: UserRegistrationDto): Promise<User | null>;
+    register(username: string): Promise<User | null>;
     logout(): void;
     getCurrentUser(): User | null;
     isLoggedIn(): Promise<boolean>;
